eb68918a15a13b1531a6be852536d42a4412377e
Aidan Doherty <aidan@thisislevelup.com>
2021-10-08 13:25:54 +0100

    remove unused import

--- android/src/main/java/com/brentvatne/react/ReactVideoView.java
+++ android/src/main/java/com/brentvatne/react/ReactVideoView.java
@@ -35,7 +35,6 @@ import com.yqritc.scalablevideoview.ScaleManager;
 import com.yqritc.scalablevideoview.Size;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
